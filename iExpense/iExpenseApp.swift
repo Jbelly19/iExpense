6a128dbe6f0dffe6a5e1f18d567f31baf0ca5b3e
Josh Belmont <jbelmont19@live.com>
2020-10-20 08:03:26 -0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Josh Belmont on 10/20/20.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
